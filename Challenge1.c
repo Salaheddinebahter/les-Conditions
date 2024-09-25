15d48441a7745bb9f40d5866a4e4a410497ef8cc
salaheddinebahter <bahtar8080@gmail.com>
2024-09-25 12:42:00 +0200

    les Condition

---
+++ Challenge1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ int main () 
+{
+ int x ;
+ printf("entre le nomber:");
+ scanf("%d",&x);
+
+ if (x%2==0){
+     printf("le nombre est pair ");
+ } 
+ else 
+     printf("le nombre est impair");
+     
+ 
+ return 0;
+}
