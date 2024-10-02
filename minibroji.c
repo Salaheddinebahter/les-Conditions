683f6ea5b8c5db6f35a34aa61f19dd649d43f337
salaheddinebahter <bahter8080@gmail.com>
2024-10-02 12:29:49 +0200

    les tableux

--- minibroji.c
+++ minibroji.c
@@ -2,14 +2,13 @@
 #include <string.h>
 
 #define max_liver 100
-int main()
-{
+
    char titre[max_liver][100];
-   int quil[max_liver] ;//quil = la quantité d'un livre.
-   char auil[max_liver];//auil= Auteur du livre. // prix=Prix du livr.
-   float prix [max_liver] ;
+   int quli[max_liver][100] ; //quli = la quantité d'un livre.
+   char auli[max_liver][100]; //auli= Auteur du livre. 
+   float prix[max_liver][100] ; // prix=Prix du livr.
    int toli=0 , nombre ; //toli=total des livers.
-   
+  int main(){ 
    printf("Menu de gestion de la bibliothèque:\n");
    printf("1.Ajouter un livre à l'inventaire:\n");
    printf("2.Voir tous les livres en stock:\n");
@@ -20,17 +19,109 @@ int main()
    printf("7.Fermer le programme:\n");
    printf("Choisissez un numéro à exécuter:");
             scanf("%d",&nombre);
-   
+  }
    void AjouterUnlivre(){
-       if(toli=0 < auil[max_liver] )
+       
+       if(toli < auli[max_liver] ){
+   
        printf("Entrez le titre du livre que vous souhaitez ajouter:\n");
-       scanf("%s\n",&titre[toli]);
-        
+         scanf("%[^\n]%*c",&titre[toli]);
+         
+       printf("Entrez l'auteur du livre que vous souhaitez ajouter:\n");
+         scanf("%[^\n]%*c",&auli[toli]);
+       
+       printf("Entrez le prix du livre que vous souhaitez ajouter:\n");
+         scanf("%f",&prix[toli]);
+         
+       printf("Entrez le nombre de livres que vous souhaitez ajouter:\n");
+         scanf("%d",&quli[toli]);
+    
+          toli++;
+   }else{
+       printf("Le stock est plein!");
    }
-   void afficherLivers()
-   void recherche()
+   
+   void afficherLivers(){
+       
+       if("toli==0"){
+           printf("Il n'y a pas de livres en stock !");
+       }else{
+           printf("Livres en stock:");
+           
+         for(int i =0 ; i>toli ; i++){
+           printf("Titre du livre:%s\n",titre[i]);
+           printf("Auteur du livre:%s\n",auli[i]);
+           printf("Prix du livre:%.2f\n",prix[i]);
+           printf("Quantité de livre:%d\n",quli[i]);
+           printf("======================================\n");
+         }
+       }
+       
+   }
+
+   void recherche(){
+       char recherche[100];
+         
+         printf("Entrez le titre du livre que vous souhaitez rechercher:\n");
+         scanf("%[^\n]%*c",recherche);
+         
+         for(i=0 ; i>toli ; i++){
+             if(strcmp(recherche,titre[i])==0){
+                 printf("Le livre a été trouvé:\n");
+                 printf("Titre du livre : %s\n",titre[i]);
+                 printf("Auteur du livre : %s\n",auli[i]);
+                 printf("Prix du livre%2.f\n",prix[i]);
+                 printf("Quantité de livre%d\n",quli[i]);
+                 return;
+         }else{
+             printf("Le livre n'a pas été trouvé!");
+         }
+         }
+   }
+
    void MettreAjour()
-   void Supprimer()
+       char MettreAjour[100];
+       int  Nouqua;    //Nouqua=Nouvelle quantité.
+       
+                 printf("Le titre du livre dont vous souhaitez mettre à jour la quantité:\n");
+           scanf("%[^\n]%*c",MettreAjour);
+           
+         for(int i = 0 ; i < toli ; i++){
+             if (strcmp(MettreAjour, titre[i])==0){
+                 printf("Entrez la nouvelle quantité de livres:\n");
+                 scanf("%d",&Nouqua);
+                   quli[i] = Nouqua;
+                 printf("La quantité de livres a été mise à jour avec succès");
+                 return;
+             }else{
+                 printf("Le livre n'a pas été trouvé. Assurez-vous que le nom est correct et réessayez.");
+             }
+         }
+          
+   void Supprimer(){
+       char Supprimer[100];
+                 printf("Entrez le titre du livre que vous souhaitez supprimer");
+                   scanf("%[^\n]%*c")
+       
+       
+       
+       
+       
+   }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
    void totallivres()
    
    switch (nombre) {
