838c603d87fa6d363ac4b615469a90b1da3db8e4
salaheddinebahter <bahter8080@gmail.com>
2024-10-02 16:22:00 +0200

    les tableux

--- minibroji.c
+++ minibroji.c
@@ -4,15 +4,15 @@
 #define max_liver 100
 
    char titre[max_liver][100];
-   int quli[max_liver][100] ; //quli = la quantité d'un livre.
+   int quli[max_liver] ; //quli = la quantité d'un livre.
    char auli[max_liver][100]; //auli= Auteur du livre. 
-   float prix[max_liver][100] ; // prix=Prix du livr.
-   int toli=0 , nombre ; //toli=total des livers.
+   float prix[max_liver] ; // prix=Prix du livr.
+   int toli=0 ; //toli=total des livers.
 
   
 void AjouterUnlivre() {
        
-      if(toli<auli[max_liver]){
+      if(toli < max_liver){
    
        printf("Entrez le titre du livre que vous souhaitez ajouter:\n");
          scanf("%[^\n]%*c",&titre[toli]);
@@ -28,17 +28,17 @@ void AjouterUnlivre() {
     
           toli++;
    }else{
-       printf("Le stock est plein!");
+       printf("Le stock est plein!\n");
    }
-   
+}
 void afficherLivers(){
        
-       if("toli==0"){
+       if(toli==0){
            printf("Il n'y a pas de livres en stock !");
        }else{
            printf("Livres en stock:");
            
-         for(int i =0 ; i>toli ; i++){
+         for(int i =0 ; i<toli ; i++){
            printf("Titre du livre:%s\n",titre[i]);
            printf("Auteur du livre:%s\n",auli[i]);
            printf("Prix du livre:%.2f\n",prix[i]);
@@ -50,26 +50,27 @@ void afficherLivers(){
    }
 
 void recherche(){
+    
        char recherche[100];
-         
+       int i;
+        
          printf("Entrez le titre du livre que vous souhaitez rechercher:\n");
          scanf("%[^\n]%*c",recherche);
          
-         for(i=0 ; i>toli ; i++){
-             if(strcmp(recherche,titre[i])==0){
+         for(i = 0 ; i < toli ; i++){
+             if(strcmp(recherche,titre[i]) == 0){
                  printf("Le livre a été trouvé:\n");
                  printf("Titre du livre : %s\n",titre[i]);
                  printf("Auteur du livre : %s\n",auli[i]);
                  printf("Prix du livre%2.f\n",prix[i]);
                  printf("Quantité de livre%d\n",quli[i]);
                  return;
-         }else{
-             printf("Le livre n'a pas été trouvé!");
+            }
          }
-         }
+         printf("Le livre n'a pas été trouvé!");
    }
 
-void MettreAjour()
+void MettreAjour(){
        char MettreAjour[100];
        int  Nouqua;    //Nouqua=Nouvelle quantité.
        
@@ -81,41 +82,41 @@ void MettreAjour()
                  printf("Entrez la nouvelle quantité de livres:\n");
                  scanf("%d",&Nouqua);
                    quli[i] = Nouqua;
-                 printf("La quantité de livres a été mise à jour avec succès");
+                 printf("La quantité de livres a été mise à jour avec succès\n");
                  return;
-             }else{
-                 printf("Le livre n'a pas été trouvé. Assurez-vous que le nom est correct et réessayez.");
              }
-         }
-         
+       
+        }
+        printf("Le livre n'a pas été trouvé. Assurez-vous que le nom est correct et réessayez.\n");
+ }        
 void Supprimer(){
     
-       char Supprimer[100];
-                 printf("Entrez le titre du livre que vous souhaitez supprimer");
-                   scanf("%[^\n]%*c",Supprimer);
-      for(int = 0 ; i < toli ;-1){
-          if(strcmp(Supprimer,titres[i])==0){ 
+       char Supprim[100];
+                 printf("Entrez le titre du livre que vous souhaitez supprimer\n");
+        scanf("%[^\n]%*c",Supprim);
+                   
+      for(int i = 0 ; i < toli-1 ; i++ ){
+          if(strcmp(Supprim,titre[i])==0){ 
               for(int j = i ; j < toli-1 ; j++){
-                  strcpy(titres[j],titres[j+1]);
-                  strcpy(aulis[j],aulis[j+1]);
+                  strcpy(titre[j],titre[j+1]);
+                  strcpy(auli[j],auli[j+1]);
                   prix[j] = prix[j+1];
                   quli[j] = quli[j+1];
               }
-              toli--
+              toli--;
               printf("Le livre a bien été supprimé.");
               return;
-          }
       }
    }
 
 void totallivres(){
-    
-       int total=0;
-       for (int i = 0 ; i<toli ; i++){
+     int total=0;
+       for (int i = 0 ; i < toli ; i++){
            total += quli[i];
        }
        printf("Nombre total de livres en stock:%d",total);
    }
+   
 int main() { 
          
     int nombre;
@@ -152,13 +153,14 @@ int main() {
          totallivres();
          break;
        case 7:
-         fermer();
+         printf("programe ferme.");
          break;
        default:
           printf("Ce numéro n'est pas dans la liste!");
-          {
-   }while (chois !=7);
+   }
+   }while (nombre != 7);
    
- 
-  return 0;  
+
+ return 0;
+    
 }
